88db21ecf349037cfb3de4407605d2b00e19b3e5
SHIVA9964599018 <38631570+SHIVA9964599018@users.noreply.github.com>
2025-03-27 18:01:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // Ensure Supabase is available
 document.addEventListener("DOMContentLoaded", function () {
