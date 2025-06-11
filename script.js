ec1b2ba83d9a9a944ee98d1e63ef932a675784c3
SHIVA9964599018 <38631570+SHIVA9964599018@users.noreply.github.com>
2025-06-11 16:48:18 +0530

    c
    cv

--- script.js
+++ script.js
@@ -1151,3 +1151,24 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+let deferredPrompt;
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = 'inline-block';
+
+  installBtn.addEventListener('click', () => {
+    installBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
