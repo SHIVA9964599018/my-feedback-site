40d26b946561bb41013069a4d34453f1ba61c5cb
SHIVA9964599018 <38631570+SHIVA9964599018@users.noreply.github.com>
2025-06-11 16:05:05 +0530

    v
    gd

--- script.js
+++ script.js
@@ -1141,5 +1141,10 @@ window.showUsernameOnTop = function (username) {
 };
 
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker registered"))
+    .catch((err) => console.log("Service Worker registration failed:", err));
+}
 
 
